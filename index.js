94ccec6ef069a03429fe4c660a18101194f11c05
sharukhk08 <iamsharukh08@gmail.com>
2022-06-20 23:06:53 +0530

    add cors and port for live

--- index.js
+++ index.js
@@ -20,12 +20,8 @@ mongoose
   }); // connect to mongo db
 
 
-const corsOptions = {
-  origin: 'https://task-node-react.herokuapp.com',
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+
+app.use(cors());
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -43,6 +39,8 @@ app.use("/api/v1", routes);
 //   console.log("Home api run");
 // });
 
-app.listen(4000, () => {
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
   console.log("App listening on port 4000!");
 });
