28f6ee17ad8f7cfcb689994c203eba074e50a079
sharukhk08 <iamsharukh08@gmail.com>
2022-06-20 22:36:01 +0530

    console

--- controllers/todolist.js
+++ controllers/todolist.js
@@ -3,7 +3,7 @@ const addTodoSchema = require("../schema/addTodo");
 const gettodolist = async (req, res) => {
   try {
     const todolist = await addTodoSchema.find({});
-
+    console.log(todolist);
     return res.status(200).json({
       status: "success",
       todos: todolist.reverse(),
